31aa4e266c6626520407277d076532ac7e6b0b52
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-10-25 16:10:11 +0530

    debugging

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED 
 pragma solidity >=0.5.0 <0.9.0;
 
+import "hardhat/console.sol";
+
 contract Token {
     string public name = " Hardhat Token";
     string public symbol = "HHT";
@@ -16,6 +18,8 @@ contract Token {
     }
 
     function transfer(address to,uint amount) external {
+        console.log("###Sender balances is %s tokens ", balances[msg.sender]);
+        console.log("###Sender is sending %s tokens to %s address ", amount, to);
         require(balances[msg.sender]>=amount,"Not enough tokens");
         balances[msg.sender] -= amount;
         balances[to]+= amount;
