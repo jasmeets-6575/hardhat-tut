b3b8966c4ebed8a530adb3b0d4c6a42b136c8bb2
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-10-25 17:57:17 +0530

    deploying

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,13 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
 require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEYs}`,
+      accounts: [`${process.env.GOERLI_PRIVATE_KEY}`],
+    },
+  },
 };
---
+++ script/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
